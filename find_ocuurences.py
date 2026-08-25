312cfd92987d52742fd99c14419823f29ac45a40
Edwin-Giteru <egiteru5@gmail.com>
2026-08-25 15:49:59 +0300

    day 2

---
+++ find_ocuurences.py
@@ -0,0 +1,10 @@
+# A method to find the occurence of a target in a list
+def count_occurrences(numbers, target):
+    count = 0
+    for num in numbers:
+        if num == target:
+            count += 1
+    return count
+
+numbers=[1,2,4,6,2,9,2,5,3,2]
+print("Number of Occurences:", count_occurrences(numbers, 2))
